6acead1e97d4f4d16c27d7fa2a9722a55c97b7cd
Kulilin <108369969@qq.com>
2024-07-26 10:46:37 +0800

    优化飞机移动

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -8,9 +8,18 @@ public class Player : MonoBehaviour
 {
     [SerializeField] PlayerInput playerInput;
     // Start is called before the first frame update
-    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float moveSpeed = 7f;
     [SerializeField] float paddingX = 0.8f;
     [SerializeField] float paddingY = 0.2f;
+
+    //加速
+    [SerializeField] float acceleationTime = 0.2f;
+    //减速
+    [SerializeField] float decelerationTime = 0.2f;
+
+    [SerializeField] float moveRotationAngle = 27f;
+
+    private Coroutine coroutine;
     new Rigidbody2D rigidbody;
 
     void Awake()
@@ -30,17 +39,28 @@ public class Player : MonoBehaviour
         playerInput.onStopMove -= StopMove;
     }
 
-    private void StopMove()
-    {
-        rigidbody.velocity = Vector2.zero;
-        StopCoroutine(MovePositionLimitCoroutine());
-    }
 
     private void OnMove(Vector2 moveInput)
     {
+        if (coroutine != null) {
+            StopCoroutine(coroutine);
+        }
         //Vector2 moveAmount = moveInput * moveSpeed;
-        rigidbody.velocity = moveInput * moveSpeed;
+        //rigidbody.velocity = moveInput * moveSpeed;
+
+        Quaternion rotation = Quaternion.AngleAxis(moveRotationAngle * moveInput.y, Vector3.right);
+        coroutine = StartCoroutine(MoveCoroutine(acceleationTime, moveInput.normalized * moveSpeed, rotation));
         StartCoroutine(MovePositionLimitCoroutine());
+    }
+
+     private void StopMove()
+    {
+        if (coroutine != null) {
+            StopCoroutine(coroutine);
+        }
+        //rigidbody.velocity = Vector2.zero;
+        coroutine = StartCoroutine(MoveCoroutine(decelerationTime, Vector2.zero, Quaternion.identity));
+        StopCoroutine(MovePositionLimitCoroutine());
     }
 
     void Start()
@@ -61,6 +81,40 @@ public class Player : MonoBehaviour
         {
             transform.position = Viewport.Instance.PlayerLimitPosition(transform.position, paddingX, paddingY);
             //yield return new WaitForSeconds(0.1f);
+            yield return null;
+        }
+    }
+
+    // IEnumerator StartMoveCoroutine(Vector2 moveAmount)
+    // {
+    //     float time = 0f;
+    //     while (time < acceleationTime)
+    //     {
+    //         time += Time.fixedDeltaTime / acceleationTime;
+    //         rigidbody.velocity = Vector2.Lerp(Vector2.zero, moveAmount, time / acceleationTime);
+    //         yield return null;
+    //     }
+    // }
+    // IEnumerator StopMoveCoroutine(Vector2 moveAmount)
+    // {
+    //     float time = 0f;
+    //     while (time < decelerationTime)
+    //     {
+    //         time += Time.fixedDeltaTime / decelerationTime;
+    //         rigidbody.velocity = Vector2.Lerp(Vector2.zero, moveAmount, time / decelerationTime);
+    //         yield return null;
+    //     }
+    // }
+    IEnumerator MoveCoroutine(float movetime, Vector2 moveAmount, Quaternion moveRotation)
+    {
+        float time = 0f;
+        while (time < movetime)
+        {
+            time += Time.fixedDeltaTime / movetime;
+            rigidbody.velocity = Vector2.Lerp(Vector2.zero, moveAmount, time / movetime);
+
+            //添加飞机旋转
+            transform.rotation = Quaternion.Lerp(transform.rotation, moveRotation, time / movetime);
             yield return null;
         }
     }
